ef9f2136e3d6622f3df37397d149312277d258e6
MSA8D8 <demo.abc2@gmail.com>
2018-06-06 23:26:10 +0900

    first comment

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 #  code:utf-8
-import os
 import io
-import re
 import sys
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
 from sklearn.cross_validation import train_test_split
 
-from preprocessing import description_vectorizer
-from preprocessing import item_description_tokenizer
 from preprocessing import label_encode
 from preprocessing import text_process
 from preprocessing import get_keras_data
@@ -19,12 +13,10 @@ from preprocessing import get_keras_data
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Input, Dropout, Dense, BatchNormalization, Activation, concatenate, GRU, Embedding, Flatten, BatchNormalization
+from sklearn.preprocessing import MinMaxScaler
+from keras.layers import Input, Dropout, Dense, concatenate, GRU, Embedding, Flatten
 from keras.models import Model
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras import backend as K
 
 def get_callbacks(filepath, patience=2):
@@ -38,10 +30,8 @@ def rmsle_cust(y_true, y_pred):
     return K.sqrt(K.mean(K.square(first_log - second_log), axis=-1))
 
 def get_model():
-    #params
     dr_r = 0.1
 
-    #Inputs
     name = Input(shape=[X_train["name"].shape[1]], name="name")
     item_desc = Input(shape=[X_train["item_desc"].shape[1]], name="item_desc")
     brand_name = Input(shape=[1], name="brand_name")
@@ -49,33 +39,26 @@ def get_model():
     item_condition = Input(shape=[1], name="item_condition")
     num_vars = Input(shape=[X_train["num_vars"].shape[1]], name="num_vars")
 
-    #Embeddings layers
     emb_name = Embedding(MAX_TEXT, 50)(name)
     emb_item_desc = Embedding(MAX_TEXT, 50)(item_desc)
     emb_brand_name = Embedding(MAX_BRAND, 10)(brand_name)
     emb_category_name = Embedding(MAX_CATEGORY, 10)(category_name)
     emb_item_condition = Embedding(MAX_CONDITION, 5)(item_condition)
 
-    #rnn layer
     rnn_layer1 = GRU(16) (emb_item_desc)
     rnn_layer2 = GRU(8) (emb_name)
 
-    #main layer
-    main_l = concatenate([
-        Flatten() (emb_brand_name)
-        , Flatten() (emb_category_name)
-        , Flatten() (emb_item_condition)
-        , rnn_layer1
-        , rnn_layer2
-        , num_vars
-    ])
+    main_l = concatenate([Flatten() (emb_brand_name)
+                          , Flatten() (emb_category_name)
+                          , Flatten() (emb_item_condition)
+                          , rnn_layer1
+                          , rnn_layer2
+                          , num_vars])
+
     main_l = Dropout(dr_r) (Dense(128) (main_l))
     main_l = Dropout(dr_r) (Dense(64) (main_l))
 
-    #output
     output = Dense(1, activation="linear") (main_l)
-
-    #model
     model = Model([name, item_desc, brand_name
                    , category_name, item_condition, num_vars], output)
     model.compile(loss="mse", optimizer="adam", metrics=["mae", rmsle_cust])
@@ -96,14 +79,10 @@ def draw_heatmap(data, row_labels, column_labels):
     ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
     ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
 
-    #ax.invert_yaxis()
-    #ax.xaxis.tick_top()
-
     ax.set_xticklabels(row_labels, minor=False,rotation=90, fontsize=10)
     ax.set_yticklabels(column_labels, minor=False,rotation=0, fontsize=8)
 
     plt.savefig('image.png')
-    #plt.show()
     return heatmap
 
 
@@ -111,18 +90,18 @@ def handle_missing(dataset):
     dataset.category_name.fillna(value="missing", inplace=True)
     dataset.brand_name.fillna(value="missing", inplace=True)
     dataset.item_description.fillna(value="missing", inplace=True)
-    return (dataset)
+    return dataset
 
 
 if __name__ == '__main__':
-    train = pd.read_csv('train.tsv',delimiter='\t')
-    test = pd.read_csv('test.tsv',delimiter='\t')
+    train = pd.read_csv('train.tsv', delimiter='\t')
+    test = pd.read_csv('test.tsv', delimiter='\t')
 
     train = handle_missing(train)
     test = handle_missing(test)
 
-    train,test = label_encode(train,test)
-    train,test = text_process(train,test)
+    train, test = label_encode(train, test)
+    train, test = text_process(train, test)
 
     max_name_seq = np.max([np.max(train.seq_name.apply(lambda x: len(x))),
                            np.max(test.seq_name.apply(lambda x: len(x)))])
--- preprocessing.py
+++ preprocessing.py
@@ -1,26 +1,13 @@
 #  code:utf-8
-import os
-import io
 import re
-import sys
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
-import matplotlib.pyplot as plt
-from nltk.corpus import stopwords
-from gensim.models import word2vec
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.preprocessing import LabelEncoder
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords
 
-
-
-
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 def get_keras_data(dataset,MAX_NAME_SEQ,MAX_ITEM_DESC_SEQ):
     X = {
@@ -44,53 +31,33 @@ def label_encode(train,test):
     train.brand_name = le.transform(train.brand_name)
     test.brand_name = le.transform(test.brand_name)
     del le
-    return train,test
+    return train, test
 
 
 def description_vectorizer(text):
-    stoplist = ['and','in','you','I','we','with','the','it','new','no','to','for','is','was','were','but','from'
-    ,'my','of','on','are','or','rm','this','will','can','could','would','have','has','me','please','as'
-    ,'all','be','like','very','at','any','an','a','been','both','by','comes','day','description','do'
-    ,'dont','fit','fits','front','if','home','item','items','its','just','more','nwt','one','out','other'
-    ,'see','so','some','still','them','these','they','too','up','your','also','each','im','inside','make'
-    ,'offers','that','two','vs','works','price']
-
-    vectorizer = TfidfVectorizer(max_df=0.5, min_df=1, max_features=10, stop_words=stoplist)
+    vectorizer = TfidfVectorizer(max_df=0.5, min_df=1,
+                                 max_features=10,
+                                 stop_words=stopwords.words('english'))
     tfidf_weighted_matrix = vectorizer.fit_transform(text)
-
-    '''
-    print(tfidf_weighted_matrix.shape)
-    print(vectorizer.get_feature_names())
-
-    for n, text in enumerate(text[0:5]):
-        print ("[%s]" % text)
-        print (u"重み: %s\n" % ",".join(["%s:%.2f" % (token, weight) for token, weight in
-                                      zip(vectorizer.get_feature_names(),
-                                          tfidf_weighted_matrix.getrow(n).toarray()[0]
-                                          )]))
-    '''
-
-    return pd.DataFrame(tfidf_weighted_matrix.toarray()),vectorizer.get_feature_names()
+    return pd.DataFrame(tfidf_weighted_matrix.toarray()), vectorizer.get_feature_names()
 
 
-def text_process(train,test):
-    raw_text = np.hstack([train.item_description.str.lower(), train.name.str.lower()])
-
-    print("   Fitting tokenizer...")
+def text_process(train, test):
+    raw_text = np.hstack([train.item_description.str.lower(),
+                          train.name.str.lower()])
     tok_raw = Tokenizer()
     tok_raw.fit_on_texts(raw_text)
-    print("   Transforming text to seq...")
-
     train["seq_item_description"] = tok_raw.texts_to_sequences(train.item_description.str.lower())
     test["seq_item_description"] = tok_raw.texts_to_sequences(test.item_description.str.lower())
     train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
     test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
-    return train,test
+    return train, test
 
 def item_description_tokenizer(text):
     word_list = [sentence.split(' ') for sentence in text]
-    word_list = [[re.sub(re.compile("[!-/:-@[-`{-~]"),'',word) for word in sentence]
-                 for sentence in word_list]
+    word_list = [[re.sub(re.compile("[!-/:-@[-`{-~]"), '', word)
+                 for word in sentence] for sentence in word_list]
+
     word_list = [[re.sub(re.compile("[0-9]+"),'',word) for word in sentence] for sentence in word_list]
     word_list = [[word for word in filter(lambda s:s!='', sentence)] for sentence in word_list]
     sentences = [' '.join(sentence) for sentence in word_list]
